603d9c4e0971956abe910267a0b4c13d3cf4eca5
Mike Bush <mpbush@gmail.com>
2013-09-29 13:26:47 +0100

    Tidy LoggerSpec a bit

--- src/test/scala/LoggerSpec.scala
+++ src/test/scala/LoggerSpec.scala
@@ -6,8 +6,10 @@ class LoggerSpec extends FlatSpec with MockFactory with ShouldMatchers {
 	"that with a String" should "log a basic instantaneous event" in {
 		val appender = mock[EventAppender]
 		val log = new Logger(appender)
+
 		(appender.append _).expects(BasicEvent("Some event happened"))
-		log.that("Some event happened").toString
+
+		log that "Some event happened"
 	}
 
 	case class Noun(noun: String)
@@ -15,25 +17,31 @@ class LoggerSpec extends FlatSpec with MockFactory with ShouldMatchers {
 	"thatThe with a noun object, verb and predicate" should "log a simple instantaneous event" in {
 		val appender = mock[EventAppender]
 		val log = new Logger(appender)
-		(appender.append _).expects(SimpleEvent(Noun("Thing"), "has done something"))
 		val subject = Noun("Thing")
-		log.thatThe(subject).has("done something")
+
+		(appender.append _).expects(SimpleEvent(subject, "has done something"))
+
+		log thatThe subject has "done something"
 	}
 
 	"the with a verb of subject" should "log an outcome event of the block" in {
 		val appender = mock[EventAppender]
 		val log = new Logger(appender)
-		(appender.append _).expects(ProcessEvent("Process", Noun("Thing"), Left("Response")))
 		val subject = Noun("Thing")
-		log.the("Process").of(subject).in("Response")
+
+		(appender.append _).expects(ProcessEvent("Process", subject, Left("Response")))
+
+		log the "Process" of subject in { "Response" }
 	}
 
 	it should "log an exception outcome event of the block" in {
 		val appender = mock[EventAppender]
 		val log = new Logger(appender)
 		val exception = new Exception
-		(appender.append _).expects(ProcessEvent("Process", Noun("Thing"), Right(exception)))
 		val subject = Noun("Thing")
+
+		(appender.append _).expects(ProcessEvent("Process", subject, Right(exception)))
+
 		intercept[Exception] {
 			log the "Process" of subject in { throw exception }
 		}
