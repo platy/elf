9dfb2b09782d37495a8650b8ee4aef64b0b167e2
Mike Bush <mpbush@gmail.com>
2013-09-29 03:20:28 +0100

    Add block log event

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -4,12 +4,29 @@ case class BasicEvent(event: String) extends Event {
 }
 
 case class SimpleEvent(subject: Any, predicate: String) extends Event {
-
+	override def toString = subject.toString + predicate
 }
+
+case class ProcessEvent(verb: String, subject: Any, result: Either[Any, Throwable]) extends Event {
+	
+}
+
 
 class SubjectLogger(eventAppender: EventAppender, subject: Any) {
 	// need to import verbs from somewhere else
 	def has(predicate: String) = eventAppender.append(SimpleEvent(subject, "has " + predicate))
+}
+
+class VerbLogger(eventAppender: EventAppender, verb: String) {
+	def of(subject: Any) = new BlockProcessLogger(eventAppender, verb, subject)
+}
+
+class BlockProcessLogger(eventAppender: EventAppender, verb: String, subject: Any) {
+	def in(block: () => Any) = eventAppender.append(try {
+		ProcessEvent(verb, subject, Left(block()))
+	} catch {
+		case ex: Throwable => ProcessEvent(verb, subject, Right(ex))
+	})
 }
 
 class Logger(eventAppender: EventAppender) {
@@ -17,4 +34,6 @@ class Logger(eventAppender: EventAppender) {
 	def that(event: String) = eventAppender.append(new BasicEvent(event))
 
 	def thatThe(subject: Any) = new SubjectLogger(eventAppender, subject)
+
+	def the(verb: String) = new VerbLogger(eventAppender, verb)
 }
--- src/test/scala/LoggerSpec.scala
+++ src/test/scala/LoggerSpec.scala
@@ -16,6 +16,24 @@ class LoggerSpec extends FlatSpec with MockFactory with ShouldMatchers {
 		val appender = mock[EventAppender]
 		val log = new Logger(appender)
 		(appender.append _).expects(SimpleEvent(Noun("Thing"), "has done something"))
-		log.thatThe(Noun("Thing")).has("done something")
+		val subject = Noun("Thing")
+		log.thatThe(subject).has("done something")
+	}
+
+	"the with a verb of subject" should "log an outcome event of the block" in {
+		val appender = mock[EventAppender]
+		val log = new Logger(appender)
+		(appender.append _).expects(ProcessEvent("Process", Noun("Thing"), Left("Response")))
+		val subject = Noun("Thing")
+		log.the("Process").of(subject).in(() => "Response")
+	}
+
+	it should "log an exception outcome event of the block" in {
+		val appender = mock[EventAppender]
+		val log = new Logger(appender)
+		val exception = new Exception
+		(appender.append _).expects(ProcessEvent("Process", Noun("Thing"), Right(exception)))
+		val subject = Noun("Thing")
+		log.the("Process").of(subject).in(() => throw exception)
 	}
 }
